1d64a0034d47ce138c89bbb7e1037d2c0989e035
LordCliff77 <cliffowusu2727@gmail.com>
2020-10-15 23:38:08 +0100

    Testing commit

--- src/components/books/BookList.jsx
+++ src/components/books/BookList.jsx
@@ -15,7 +15,7 @@ export default () => {
 		</BooksWrapper>
 	) : (
 		<EmptyBooks>
-			You are currently not reading anything. Please add a book.<strong> Na reading de3 3y3</strong>
+			You are currently not reading anything. Please add a book.<strong> Na reading de3 3y3 paa</strong>
 		</EmptyBooks>
 	);
 };
